b05f4b6ffdd357efed95fceaad3e902d74288938
jimmynetor3 <54798823+jimmynetor3@users.noreply.github.com>
2020-06-18 14:22:17 +0200

    af

---
+++ urlchecker.php
@@ -0,0 +1,36 @@
+<?php
+
+$patternProtocol = '/[a-z]{1,}[":"]/m';
+$patternDomain = '/[a-z]{1,}["."]{1}[a-z]{2,}/m';
+$patternPath = '/["\/"].{1,}/m';
+$input = $argv[1];
+$offset = 0;
+if (preg_match($patternProtocol, $input, $matches)) {
+    $NumberOfLetters = strlen($matches[0]);
+    $offset = $offset + $NumberOfLetters;
+
+    foreach ($matches as $word) {
+        echo("protocol : " . $word . PHP_EOL);
+    }
+} else {
+    echo("geen protocol gevonden" . PHP_EOL);
+}
+
+if (preg_match($patternDomain, $input, $matches)) {
+    $NumberOfLetters = strlen($matches[0]);
+    $offset = $offset + $NumberOfLetters;
+    foreach ($matches as $word) {
+        echo("domain : " . $word . PHP_EOL);
+    }
+} else {
+    echo("no domain found het moet wel een werkende url zijn" . PHP_EOL);
+    exit();
+}
+
+if (preg_match($patternPath, $input, $matches, PREG_OFFSET_CAPTURE, $offset)) {
+    foreach ($matches as $word) {
+        echo("path : " . $word[0] . PHP_EOL);
+    }
+}else{
+    echo("no path found");
+}
